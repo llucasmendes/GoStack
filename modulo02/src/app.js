1f35abc51866e322a044b9b52b76916e5a8ae4e3
llucasmendes <llucasmendes@hotmail.com>
2019-10-21 11:32:51 -0300

    Configurando Eslint
    Configuração do editor com
    -Eslint
    - Prettier
    - Editor_config

--- modulo02/src/app.js
+++ modulo02/src/app.js
@@ -1,19 +1,21 @@
 import express from 'express';
 import routes from './routes';
 
-class App{
-    constructor(){
-        this.server = express();
+class App {
+  constructor() {
+    this.server = express();
 
-        this.middlewares();
-        this.routes();
-    }
-    middlewares(){
-        this.server.use(express.json());
-    }
-    routes(){
-        this.server.use(routes);
-    }
+    this.middlewares();
+    this.routes();
+  }
+
+  middlewares() {
+    this.server.use(express.json());
+  }
+
+  routes() {
+    this.server.use(routes);
+  }
 }
 
 export default new App().server;
