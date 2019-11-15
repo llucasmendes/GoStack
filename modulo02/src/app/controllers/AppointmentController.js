8ba814b41db1c0b3400a9c2cb43d427fa8c66582
llucasmendes <llucasmendes@hotmail.com>
2019-11-14 22:30:24 -0300

    Notificando novos agendamentos

--- modulo02/src/app/controllers/AppointmentController.js
+++ modulo02/src/app/controllers/AppointmentController.js
@@ -1,8 +1,11 @@
 import * as Yup from 'yup';
-import { startOfHour, parseISO, isBefore } from 'date-fns';
+import { startOfHour, parseISO, isBefore, format } from 'date-fns';
+import pt from 'date-fns/locale/pt';
+
 import User from '../models/User';
 import File from '../models/File';
 import Appointment from '../models/Appointment';
+import Notification from '../schemas/Notification';
 
 class AppointmentController {
   async index(req, res) {
@@ -70,6 +73,17 @@ class AppointmentController {
     /**
      * Check date availability
      */
+    const user = await User.findByPk(req.userId);
+    const formattedDate = format(
+      hourStart,
+      "'dia' dd 'de' MMMM', às' H:mm'h'",
+      { locale: pt }
+    );
+
+    await Notification.create({
+      content: `Novo angendamento de ${user.name} para ${formattedDate}`,
+      user: provider_id,
+    });
 
     const checkAvailability = await Appointment.findOne({
       where: {
@@ -90,6 +104,9 @@ class AppointmentController {
       provider_id,
       date: hourStart,
     });
+    /**
+     * Notificar prestador de serviço
+     */
 
     return res.json(appointment);
   }
--- modulo02/src/database/index.js
+++ modulo02/src/database/index.js
@@ -23,9 +23,10 @@ class Database {
       .map(model => model.associate && model.associate(this.connection.models));
   }
 
+  // admin:eNRK3jm0bs4BV@ 'mongodb://192.168.0.100:27017/gobarber'
   mongo() {
     this.mongoConnection = mongoose.connect(
-      'mongodb://192.168.0.100:27017/gobarber',
+      'mongodb://omnistack:omnistack@cluster0-shard-00-00-bubnn.mongodb.net:27017,cluster0-shard-00-01-bubnn.mongodb.net:27017,cluster0-shard-00-02-bubnn.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority',
       {
         useNewUrlParser: true,
         useFindAndModify: true,
