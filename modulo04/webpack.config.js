f8cc34c08665a6cb63686bd4f2d4e705ce5e227b
llucasmendes <llucasmendes@hotmail.com>
2019-11-18 11:15:11 -0300

    [modulo04] Importando CSS

--- modulo04/webpack.config.js
+++ modulo04/webpack.config.js
@@ -17,6 +17,13 @@ module.exports = {
         use: {
           loader: 'babel-loader',
         }
+      }, {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        use: [
+          { loader: 'style-loader' },
+          { loader: 'css-loader' },
+        ]
       }
     ]
   }
