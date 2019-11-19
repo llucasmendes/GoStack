4d990dd69a903be9c55674e819ef402c389653b7
llucasmendes <llucasmendes@hotmail.com>
2019-11-19 17:36:32 -0300

    [desafio03] Estrutura Inicial

---
+++ desafio03/src/database/index.js
@@ -0,0 +1,36 @@
+import Sequelize from 'sequelize';
+import mongoose from 'mongoose';
+
+import User from '../app/models/User';
+import File from '../app/models/File';
+import Appointment from '../app/models/Appointment';
+
+import databaseConfig from '../config/database';
+
+const models = [User, File, Appointment];
+
+class Database {
+  constructor() {
+    this.init();
+    this.mongo();
+  }
+
+  init() {
+    this.connection = new Sequelize(databaseConfig);
+
+    models
+      .map(model => model.init(this.connection))
+      .map(model => model.associate && model.associate(this.connection.models));
+  }
+
+  // admin:eNRK3jm0bs4BV@ 'mongodb://192.168.0.100:27017/gobarber'
+  mongo() {
+    this.mongoConnection = mongoose.connect(process.env.MONGO_URL, {
+      useNewUrlParser: true,
+      useFindAndModify: true,
+      useUnifiedTopology: true,
+    });
+  }
+}
+
+export default new Database();
