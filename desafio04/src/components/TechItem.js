a0bd3d36fde9f04933faf00e47706f31796bbefa
llucasmendes <llucasmendes@hotmail.com>
2019-11-19 16:15:57 -0300

    [modulo04] Reestruturando

---
+++ desafio04/src/components/TechItem.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+function TechItem({tech, onDelete}) {
+  return (
+    <li>
+      {tech}
+      <button onClick={onDelete} 
+              type="button">Remover
+      </button>
+    </li>
+  );
+}
+
+TechItem.defaultProps = {
+  tech: 'oculto',
+};
+
+TechItem.PropTypes = {
+  tech: PropTypes.string,
+  onDelete: PropTypes.func.isRequired,
+};
+
+export default TechItem;
