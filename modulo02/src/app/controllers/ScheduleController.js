40014087dc2fd8add1628b90363a16206e479665
llucasmendes <llucasmendes@hotmail.com>
2019-12-21 10:32:37 -0300

    [modulo09][modulo02] Ajustes na API

--- modulo02/src/app/controllers/ScheduleController.js
+++ modulo02/src/app/controllers/ScheduleController.js
@@ -25,6 +25,12 @@ class ScheduleController {
           [Op.between]: [startOfDay(parsedDate), endOfDay(parsedDate)],
         },
       },
+      include: [
+      {
+        model: User,
+        as: 'user',
+        attributes: ['name'],
+      }],
       order: ['date'],
     });
 
