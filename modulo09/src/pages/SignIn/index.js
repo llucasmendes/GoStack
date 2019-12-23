6f213d75cd3ab99e238e9902ca6670cf33f35402
llucasmendes <llucasmendes@hotmail.com>
2019-12-23 18:29:36 -0300

    [modulo09] Loading da autenticação

--- modulo09/src/pages/SignIn/index.js
+++ modulo09/src/pages/SignIn/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { Form, Input } from '@rocketseat/unform';
 import * as Yup from 'yup';
 
-import { signInRequest } from '~/store/modules/auth/actions';
-
 import logo from '~/assets/logo.svg';
+
+import { signInRequest } from '~/store/modules/auth/actions';
 
 const schema = Yup.object().shape({
   email: Yup.string()
@@ -14,9 +14,9 @@ const schema = Yup.object().shape({
     .required('O e-mail é obrigatório'),
   password: Yup.string().required('A senha é obrigatória'),
 });
-
 export default function SignIn() {
   const dispatch = useDispatch();
+  const loading = useSelector(state => state.auth.loading);
 
   function handleSubmit({ email, password }) {
     dispatch(signInRequest(email, password));
@@ -27,15 +27,15 @@ export default function SignIn() {
       <img src={logo} alt="GoBarber" />
 
       <Form schema={schema} onSubmit={handleSubmit}>
-        <Input name="email" type="email" placeholder="seu e-mail" />
+        <Input name="email" type="email" placeholder="Seu e-mail" />
         <Input
           name="password"
           type="password"
-          placeholder="sua senha secreta"
+          placeholder="Sua senha secreta"
         />
 
-        <button type="submit">Acessar</button>
-        <Link to="/register">Criar conta gratúita</Link>
+        <button type="submit">{loading ? 'Carregando...' : 'Acessar'}</button>
+        <Link to="/register">Criar conta gratuita</Link>
       </Form>
     </>
   );
