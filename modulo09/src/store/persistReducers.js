e23ad9c6cb7fb6f2b49921d89164df58bc184799
llucasmendes <llucasmendes@hotmail.com>
2019-12-23 22:48:42 -0300

    [modulo09] Atualizando perfil

--- modulo09/src/store/persistReducers.js
+++ modulo09/src/store/persistReducers.js
@@ -4,7 +4,7 @@ import { persistReducer } from 'redux-persist';
 export default reducers => {
   const persistedReducer = persistReducer(
     {
-      key: 'goBarber',
+      key: 'gobarber',
       storage,
       whitelist: ['auth', 'user'],
     },
