5a45ecc9fe295f83c9cc5c619b95be8db740f90a
llucasmendes <llucasmendes@hotmail.com>
2019-12-25 17:01:24 -0300

    [desafio09] Páginas e rotas

---
+++ desafio09/src/routes/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+// import Route from './Route';
+
+import SignIn from '../pages/SignIn';
+
+import Applications from '../pages/Applications';
+import Enrollments from '../pages/Enrollments';
+import Plans from '../pages/Plans';
+import Students from '../pages/Students';
+
+export default function Routes() {
+  return (
+    <Switch>
+      <Route path="/" exact component={SignIn} />
+
+      <Route path="/applications" component={Applications} isPrivate />
+      <Route path="/enrollments" component={Enrollments} isPrivate />
+      <Route path="/plans" component={Plans} isPrivate />
+      <Route path="/students" component={Students} isPrivate />
+    </Switch>
+  );
+}
