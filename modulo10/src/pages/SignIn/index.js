4f7e202e57572a4a47fc2d5fd0e8104e3a960eff
llucasmendes <llucasmendes@hotmail.com>
2019-12-25 11:51:43 -0300

    [modulo10] Input & Button

--- modulo10/src/pages/SignIn/index.js
+++ modulo10/src/pages/SignIn/index.js
@@ -1,8 +1,23 @@
 import React from 'react';
-import { View } from 'react-native';
+import { Text } from 'react-native';
+
+import Bacground from '~/components/Bacground';
+import Button from '~/components/Button';
+import Input from '~/components/Input';
 
 // import { Container } from './styles';
 
 export default function SignIn() {
-  return <View />;
+  return (
+    <Bacground>
+      <Text>Sign In</Text>
+
+      <Input
+        style={{ marginTop: 30 }}
+        icon="call"
+        placeholder="Digite seu nome"
+      />
+      <Button>Entrar</Button>
+    </Bacground>
+  );
 }
---
+++ modulo10/src/routes.js
@@ -0,0 +1,11 @@
+import { createAppContainer, createSwitchNavigator } from 'react-navigation';
+
+import SignIn from './pages/SignIn';
+import SingUp from './pages/SingUp';
+
+export default createAppContainer(
+  createSwitchNavigator({
+    SignIn,
+    SingUp,
+  })
+);
