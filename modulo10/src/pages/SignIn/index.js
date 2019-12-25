c52dd3d82cdafe30f7be5beb3805ad7501678b29
llucasmendes <llucasmendes@hotmail.com>
2019-12-25 14:36:34 -0300

    [modulo10] Página SignIn

--- modulo10/src/pages/SignIn/index.js
+++ modulo10/src/pages/SignIn/index.js
@@ -1,23 +1,44 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Image } from 'react-native';
+
+import logo from '~/assets/logo.png';
 
 import Bacground from '~/components/Bacground';
-import Button from '~/components/Button';
-import Input from '~/components/Input';
 
-// import { Container } from './styles';
+import {
+  Container,
+  Form,
+  FormInput,
+  SubmitButton,
+  SignLink,
+  SingLinkText,
+} from './styles';
 
 export default function SignIn() {
   return (
     <Bacground>
-      <Text>Sign In</Text>
+      <Container>
+        <Image source={logo} />
 
-      <Input
-        style={{ marginTop: 30 }}
-        icon="call"
-        placeholder="Digite seu nome"
-      />
-      <Button>Entrar</Button>
+        <Form>
+          <FormInput
+            icon="mail-outline"
+            keyboardType="email-address"
+            autoCorrect={false}
+            autoCapitalize="none"
+            placeholder="Digite seu e-mail"
+          />
+          <FormInput
+            icon="lock-outline"
+            secureTextEntry
+            placeholder="Digite sua senha"
+          />
+          <SubmitButton onPress={() => {}}>Acessar</SubmitButton>
+        </Form>
+        <SignLink onPress={() => {}}>
+          <SingLinkText>Criar Conta Gratuita</SingLinkText>
+        </SignLink>
+      </Container>
     </Bacground>
   );
 }
